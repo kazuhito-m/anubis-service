dd0c170f8e7303eadb4671dfbda0cb95f2242fa8
kazuhito-m <kazuhito.sumpic@gmail.com>
2014-07-29 04:23:08 +0900

    Refactored. Moved testdata to TestDataHelper.

--- src/test/scala/com/github/kazuhito_m/anubisservice/formatter/AnubisFormatterSpec.scala
+++ src/test/scala/com/github/kazuhito_m/anubisservice/formatter/AnubisFormatterSpec.scala
@@ -21,30 +21,13 @@ class AnubisFormatterSpec extends JUnitSuite {
   @Test
   def 抽象データ型を独自形式テキストに変換() {
 
-      // HTMLの元となるデータ1
+      // AnubisTextの元となるデータ1
       val baseTree = createBaseTreeForTest()
 
       val actual = sut.format(baseTree)
 
       // 結果確認
-      assert(actual === """0:'root'
- 1:'11'
-  2:'12'
-   3:'13'
-    4:'14'
-    4:'other'
-   3:'16'
-    4:'17'
-  2:'18'
-   3:'19'
-    4:'20'
-  2:'other'
-   3:'19'
-    4:'21'
- 1:'other'
-  2:'12'
-   3:'19'
-    4:'last'""")
+      assert(actual === resultAnubisText)
 
   }
 
